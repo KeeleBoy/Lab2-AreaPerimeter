b4bbc5e59073cb2676b260e0d249b4f7f69632cf
kyle <kwarchuck@gmail.com>
2020-01-22 09:44:11 -0500

    First commit

---
+++ src/lab2/areaApp.java
@@ -0,0 +1,48 @@
+package lab2;
+
+import java.util.Scanner;
+
+/*
+ * @Author Kyle Warchuck
+ */
+
+public class areaApp {
+
+	public static void main(String[] args) {
+		char userInput = 'y';
+		while (userInput == 'y') {
+		
+		Scanner scnr = new Scanner(System.in);
+		
+		double area;
+		double perimeter;
+		double volume;
+
+		System.out.println("Enter the length: ");
+		double length = scnr.nextDouble();
+		
+		System.out.println("Enter the width: ");
+		double width = scnr.nextDouble();
+		
+		System.out.println("Enter the height");
+		double height = scnr.nextDouble();
+
+		area = (length * height);
+		perimeter = (length * 2) + (height * 2);
+		volume = (height * width * length);
+
+		System.out.println("Area is " + area);
+		System.out.println("Perimeter is " + perimeter);
+		System.out.println("Volume is " + volume);
+		
+		
+		System.out.println("Would you like to run this program again? y or n: ");
+		userInput = scnr.next().charAt(0);
+		
+		
+		}
+	}
+
+}
+
+
